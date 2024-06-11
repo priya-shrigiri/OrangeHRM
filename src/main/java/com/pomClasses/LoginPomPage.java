52969a7c2c0f19c64dbf995a3187cdd067a00f61
priya-shrigiri <piashrigiri@gmail.com>
2024-06-11 18:53:43 +0530

    LogIn page SetUp

--- src/main/java/com/pomClasses/LoginPomPage.java
+++ src/main/java/com/pomClasses/LoginPomPage.java
@@ -6,10 +6,14 @@ import org.openqa.selenium.support.PageFactory;
 
 import com.module.BaseClass;
 
+import dev.failsafe.internal.util.Assert;
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
 public class LoginPomPage extends BaseClass{
 	
+
 	public LoginPomPage() {
-		PageFactory.initElements(getWebDriver(), this);
+		PageFactory.initElements(driver, this);
 	}
 	
 	@FindBy(xpath="//p[text()='Username : Admin']")
@@ -27,6 +31,42 @@ public class LoginPomPage extends BaseClass{
 	@FindBy(xpath="//button[@type='submit']")
 	private WebElement login;
 	
+	@FindBy(xpath="(//p[@class='oxd-text oxd-text--p oxd-alert-content-text'])[1]")
+	private WebElement invalidCredentials;
 	
+	@FindBy(xpath="//h6[normalize-space()='Dashboard']")
+	private WebElement dashBoard;
+	
+	public String getUsername() {
+		return userNameText.getText().substring(11);
+	}
+	public String getPassword() {
+		return passwordText.getText().substring(11);
+	}
+	
+	public void setUsername() {
+		userName.sendKeys(getUsername());
+	}
+	
+	public void setPassword() {
+		password.sendKeys(getPassword());
+	}
+	
+	public void logIn() {
+		login.click();
+	}
+   
+	public void testForInvalidCredential() {
+		String actualMsg=invalidCredentials.getText();
+		System.out.println(actualMsg + "Please Enter valid UserId And Password"); 
+	}	
+	
+	public void testForSuccessfulLogin() {
+		String requiredMsg="Dashboard";
+		String actualMsg=dashBoard.getText();
+		Assert.isTrue(true, actualMsg,requiredMsg );
+		System.out.println("*****WELCOME*****");
+	}
+	
+	}
 
-}
